e0142a0ee75cb80588ac66727cc61b5dcc14e373
Hasan <Hasan@HDAG>
2024-03-05 21:07:07 +0300

    Add project files.

---
+++ Turkcell_Kitaplik/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Turkcell_Kitaplik
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("*******************************************************************");
+            Console.WriteLine("");
+            Console.WriteLine("**  Türkçe Kitaplar Kategorisi  **  Yabancı Kitaplar Kategorisi  **");
+            Console.WriteLine();
+
+
+            Console.Read();
+        }
+    }
+}
